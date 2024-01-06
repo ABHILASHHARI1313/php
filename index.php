f4f860edde421264164bd0b6ccc73175c0e4d8fe
Abhilash Hari <abhilashhari1313@gmail.com>
2024-01-06 18:33:25 +0530

    New

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$name =  "Abhilash";
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php
+    print "My Name is " . $name . "<br>";
+    ?></h1>
+</body>
+</html>
+<?php
+
+?>
